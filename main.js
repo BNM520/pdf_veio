ec4adfd15fc5c3edd1c242d99c303986ae23995e
BNM520 <81311092+BNM520@users.noreply.github.com>
2024-04-11 15:17:11 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ function searchPdf() {
     const input = document.getElementById('searchInput');
     const filename = input.value.trim();
     const pdfUrl = 'pdf/' + filename + '.pdf';
+    const iframe = document.createElement('iframe');
+    iframe.src = `https://pdf-viewer.oss-cn-hangzhou.aliyuncs.com/web/viewer.html?file=${encodeURIComponent(pdfUrl)}`;
+    iframe.width = '100%';
+    iframe.height = '600px'; // Adjust height as needed
+    iframe.style.border = 'none';
     const pdfContainer = document.getElementById('pdfContainer');
-    pdfContainer.innerHTML = `<iframe src="https://docs.google.com/viewer?url=${encodeURIComponent(pdfUrl)}&embedded=true" width="100%" height="600px" style="border: none;"></iframe>`;
+    pdfContainer.innerHTML = '';
+    pdfContainer.appendChild(iframe);
 }
